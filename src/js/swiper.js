99c27d36db0a0c63e41b428ce35f925f9b1e3a4c
Mayagozel01 <mayagozeljumakulyyewa55@gmail.com>
2024-10-23 07:59:29 +0500

    slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,21 +1,22 @@
-const swiper = new Swiper('.myswiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+var galleryTop = new Swiper('.gallery-top', {
+  slidesPerView: 1,  
+  loop: true,
+  loopedSlides: 50,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+var galleryThumbs = new Swiper('.gallery-thumbs', {
+  direction: 'vertical',
+  slidesPerView: 4,
+  slideToClickedSlide: true,
+  scrollbar: true,
+  spaceBetween: 10,
+  loopedSlides: 50,
+  loop: true,
+
+});
+
+galleryTop.controller.control = galleryThumbs;
+galleryThumbs.controller.control = galleryTop;
